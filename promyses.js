f8df63d22a3374c81b4224f8d8a6acdd675e821a
Alfonzo <alfonsoluisarrieta253@gmail.com>
2020-05-26 02:14:15 -0500

    Varias peticiones a una API

--- promyses.js
+++ promyses.js
@@ -11,7 +11,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
         return data;
 
     })
-    .catch((error) => console.error(error))
+    .catch((error) => console.error(error));
 
 
 fetch('https://rickandmortyapi.com/api/character/')
@@ -19,13 +19,80 @@ fetch('https://rickandmortyapi.com/api/character/')
         return response.json();
     })
     .then((data) => {
-        //console.log(data);
-        console.log(`Hola, soy ${data.results[0].name}`);
-        //const name = data.results[0].name;
+        console.log(data);
+    })
+    .catch((err) => console.error(err));
+
+const URL = 'https://yts.mx/api/v2/list_movies.json';
+fetch(URL)
+    .then((response) => {
+        //console.log(response);
+        return response.json();
+    })
+    .then((data) => {
         return data;
     })
     .then((data) => {
-        console.log(`Soy originario de: ${data.results[0].origin.name}`);
-        //const origin = data.results[0].origin.name;
+        console.log(data)
     })
-    .catch((err) => console.error(err))
+    .catch((err) => console.error(err));
+
+const promise = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('Todo ok por aquí!')
+            }, 1000);
+        } else {
+            reject(new Error('Whooops!'));
+        }
+    });
+}
+
+
+promise()
+    .then((resolve) => console.log(resolve))
+    .catch((err) => console.error(err));
+
+fetch('https://rickandmortyapi.com/api/character/')
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.info.count);
+        return data;
+    })
+    .then(data => {
+        console.log(data.results[0].name);
+        return data;
+    })
+    .then(data =>
+        console.log(data.results[0].origin.name)
+    )
+    .catch(err => console.error(err));
+
+
+const url_api = 'https://rickandmortyapi.com/api/character/';
+const getCharacter = (url_api) => {
+    return new Promise((resolve, reject) => {
+        fetch(url_api)
+            .then(response => {
+                (response.status === 200) ?
+                resolve(response.json()): reject(new Error(`Ha ocurrido un error : ${url_api}`))
+            })
+    });
+};
+
+getCharacter(url_api)
+    .then(response => {
+        console.log(`Hola, soy ${response.results[0].name}`);
+        return response
+    })
+    .then(response => {
+        console.log(`Nro. de episodios: ${response.results[0].episode.length}`);
+        return response;
+    })
+    .then(response => {
+        console.log(`Origen: ${response.results[0].origin.name}`);
+    })
+    .catch(err => console.error(err));
