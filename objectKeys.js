0b6c6f527614e10aaefaca59fec0239edb67933f
Alfonso Luis Arrieta Hernández <alfonsoluisarrieta253@gmail.com>
2020-06-18 12:48:18 -0500

    ObjectKeys

---
+++ objectKeys.js
@@ -0,0 +1,30 @@
+/**
+ * devuelve un array de las propiedades namesde 
+ * un objeto, en el mismo orden como se obtienen 
+ * en un loop normal 
+ * */
+
+const user = {
+    nombre: 'pedrito',
+    apellido: 'perez',
+    edad: '10 años',
+    sexo: 'masculino',
+    ocupacion: 'estudiante'
+}
+
+//obtener propiedades 
+
+console.log(Object.keys(user));
+
+
+//imprimir propiedades y valores de un objeto.
+Object.keys(user).forEach(function viewProps(value, index, array) {
+    console.log(`La propiedad ${value} tiene el valor: ${user[value]}`);
+});
+
+//obteniendo valores del objeto user.
+const values = Object.keys(user).map(key => user[key]);
+console.log(values);
+
+const u = Object.values(user);
+console.log(u);
